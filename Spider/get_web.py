9a0574ed90fefbf2440b96a6bc866df542cdf786
MapleLeaf456 <1991418631@qq.com>
2023-06-24 18:10:38 +0800

    update

--- Spider/get_web.py
+++ Spider/get_web.py
@@ -20,5 +20,5 @@ def get_web_driver():
     chromedriver = "/usr/bin/chromedriver"
     os.environ["webdriver.chrome.driver"] = chromedriver
     driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chrome_options)
-    driver.implicitly_wait(10) # 所有的操作都可以最长等待10s
+    driver.implicitly_wait(5) # 所有的操作都可以最长等待10s
     return driver
